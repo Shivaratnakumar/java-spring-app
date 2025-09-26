c112ecc53ef17f360a06c5f11181e75122ea5780
Shivaratnakumar <shivaratnakumarpatil@gmail.com>
2025-09-27 02:54:07 +0530

    spring app - first commit

---
+++ src/main/java/net/javaguide/spring/WelcomeController.java
@@ -0,0 +1,13 @@
+package net.javaguide.spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "welcome back to Java and the world of spring!!";
+    }
+}
